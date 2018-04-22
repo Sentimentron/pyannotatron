6ae7cbccd235b3eea86388ba714c4768ce69755e
Richard Townsend <richard@sentimentron.co.uk>
2018-04-22 19:27:04 +0100

    setup: bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pyannotatron',
-    version='0.1.0',
+    version='0.2.0',
     description='Python bindings for Annotatron - a data management system for machine learning applications',
     long_description=long_description,
     url='https://github.com/Sentimentron/pyannotatron',
